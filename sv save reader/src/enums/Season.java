1698e88eebc6e5cb4a88285b40f6c88932267da5
Pema <rasoanaivo.pema@gmail.com>
2024-03-07 10:57:33 +0100

    Added a function to get an array of Seasons

--- sv save reader/src/enums/Season.java
+++ sv save reader/src/enums/Season.java
@@ -2,6 +2,22 @@ package enums;
 
 public enum Season {
 	
-	SPRING, SUMMER, FALL, WINTER
+	SPRING, SUMMER, FALL, WINTER;
+	
+	public static Season[] getSeasonsFromString(String description) {
+		
+		Season[] seasons = new Season[description.length() - description.replace(" ", "").length() + 1];
+		int indice = 0;
+		while (description.contains(" ")){
+			String seasonString = description.substring(0, description.indexOf(" "));
+			Season s = Season.valueOf(seasonString.toUpperCase());
+			description = description.substring(description.indexOf(" ")+1);
+			seasons[indice] = s; indice++;
+		}
+		seasons[indice] = Season.valueOf(description.toUpperCase());
+		
+		return seasons;
+		
+	}
 
 }
